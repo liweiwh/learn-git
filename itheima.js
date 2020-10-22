e19f6d72ed75955e90d2b1d53c69db3394e99884
LiBo <2678808390@qq.com>
2020-10-22 17:02:56 +0800

    hh

--- itheima.js
+++ itheima.js
@@ -1,32 +1,29 @@
 const hucheng = () => {
-  console.log('汪熙');
-  console.log('今天也要加油鸭');
+  console.log('汪熙')
+  console.log('今天也要加油鸭')
 }
 
 export default hucheng
 
 const woshi = () => {
-  console.log('大家好，我是冲突')
+  console.log('大家好，我是卢本伟')
 }
 export default woshi
 
-
-
 const zhangao = () => {
-  console.log('zhangao');
+  console.log('zhangao')
 }
 export default zhangao
 const didi = '666666'
 
 const songjunhua = () => {
-  console.log("大家好，我是宋俊华");
-  function add () {
-    console.log(11111111111);
+  console.log('大家好，我是宋俊华')
+  function add() {
+    console.log(11111111111)
   }
   const didi = '666666'
   const ZBM = '大家好我是张柏明'
 
-
-  const aa = "不要再删我的代码"
+  const aa = '不要再删我的代码'
   const ZBM = '大家好我是张柏明'
 }
