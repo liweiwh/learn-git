9ef66d739cdeb3d2ebf4a2fc5199bea3e288cdd7
zhangbaiming <973813763@qq.com>
2020-10-22 16:54:17 +0800

    wanl

--- itheima.js
+++ itheima.js
@@ -1,32 +1,27 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 const woshi=()=>{
   console.log('大家好，我是冲突')
 }
 export default woshi
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 111fda619c32735b4463f5a234a8a1c30e384315
-=======
+
+
+
 const zhangao = () => {
  console.log('zhangao');
 }
 export default zhangao
 const didi = '666666'
->>>>>>> 82e25bae8cd01999fc53ecc35d9c0aaaa7bbaef6
+
 const songjunhua = () => {
   console.log("大家好，我是宋俊华");
 function add () {
   console.log(11111111111);
 }
 const didi = '666666'
-<<<<<<< HEAD
 const ZBM = '大家好我是张柏明'
->>>>>>> 6684433a6e68e98fa65e84a98935a1f426bed73f
-=======
+
+
 const aa="不要再删我的代码"
 const ZBM = '大家好我是张柏明'
 }
->>>>>>> 111fda619c32735b4463f5a234a8a1c30e384315
+
