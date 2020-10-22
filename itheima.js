9aa8bd45c274b1934ba4701d27272f8951dc9b0a
zzx <2792133937@qq.com>
2020-10-22 16:54:11 +0800

    提交第三次pi

--- itheima.js
+++ itheima.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
 const a = (b) => {
  return b
-}
-
-const songjunhua=()=>{
-=======
-const songjunhua = () => {
->>>>>>> 111fda619c32735b4463f5a234a8a1c30e384315
-  console.log("大家好，我是宋俊华");
-function add () {
-  console.log(11111111111);
-}
-const didi = '666666'
-const aa="不要再删我的代码"
-const ZBM = '大家好我是张柏明'
 }
