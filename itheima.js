d469b20d19555bfdc86b4a91bad4504ffcf0d955
小艾同学啊 <gjy_0530@126.com>
2020-10-22 17:01:09 +0800

    add

--- itheima.js
+++ itheima.js
@@ -1,12 +1,9 @@
 const hucheng = () => {
-<<<<<<< HEAD
-  console.log('hucheng');
+  console.log('晚上吃啥呢?');
   console.log(1111111);
   console.log('asdasf');
 
-=======
   console.log('汪熙');
->>>>>>> 7cfffe28e11de34b392057106cca149977c3c924
   console.log('今天也要加油鸭');
 }
 
