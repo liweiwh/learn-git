e68385e3049aab76c406d3dfc5653f2ce6f94c35
gaoqing <774766080@qq.com>
2020-10-22 17:01:29 +0800

    11111

--- itheima.js
+++ itheima.js
@@ -1,13 +1,11 @@
 const hucheng = () => {
-<<<<<<< HEAD
   console.log('hucheng');
   console.log(1111111);
   console.log('asdasf');
 
-=======
   console.log('汪熙');
->>>>>>> 7cfffe28e11de34b392057106cca149977c3c924
   console.log('今天也要加油鸭');
+  console.log('吃吃吃吃吃吃吃');
 }
 
 export default hucheng
